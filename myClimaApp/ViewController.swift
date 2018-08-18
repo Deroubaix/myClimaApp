2df2a9b41a4b1b312e16270a9321d342727adff5
Marisha <marishaderoubaix@marishas-macbook-pro.local>
2018-08-18 16:49:12 +0100

    Initial Commit

---
+++ myClimaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myClimaApp
+//
+//  Created by Marisha Deroubaix on 18/08/18.
+//  Copyright © 2018 Marisha Deroubaix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
