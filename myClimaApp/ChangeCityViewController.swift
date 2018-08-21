46b996183899fb1ccbf8b379ab790d85e0c1ae10
Marisha <marishaderoubaix@marishas-macbook-pro.local>
2018-08-21 13:58:19 +0100

    Commit xcode project including Pods

---
+++ myClimaApp/ChangeCityViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChangeCityViewController.swift
+//  myClimaApp
+//
+//  Created by Marisha Deroubaix on 20/08/18.
+//  Copyright © 2018 Marisha Deroubaix. All rights reserved.
+//
+
+import UIKit
+
+protocol ChangeCityDelegate {
+  
+  func userEnteredANewCityName(city: String)
+  
+}
+
+class ChangeCityViewController: UIViewController {
+  
+  var delegate : ChangeCityDelegate?
+
+  @IBOutlet weak var changeCityTextField: UITextField!
+  
+
+  @IBAction func getWeatherPressed(_ sender: UIButton) {
+    
+    let cityName = changeCityTextField.text!
+    
+    delegate?.userEnteredANewCityName(city: cityName)
+    
+    self.dismiss(animated: true, completion: nil)
+    
+  }
+  
+  @IBAction func backButtonPressed(_ sender: UIButton) {
+    
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+}
